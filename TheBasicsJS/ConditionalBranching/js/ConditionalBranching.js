7e1e754b69b0c3418a8d0192de8f7cd2a857264e
nikmel238 <man.nikita0@yandex.ru>
2022-09-25 21:09:27 +0300

    add ConditionalBranching

---
+++ TheBasicsJS/ConditionalBranching/js/ConditionalBranching.js
@@ -0,0 +1,52 @@
+"use strict"
+
+// Конструкция if ... else if ... else
+let calc = +prompt (`5 + 5 = ?`, ``);
+
+if (calc === 10) {
+  alert(`Ты гений!`);
+} else if (calc > 10) {
+  alert(`Это много. Учи математику!`);
+} else if (calc < 10) {
+  alert(`Это мало. Учи математику!`);
+} else {
+  alert(`Нужно было писать арабской цифрой!!!`);
+}
+
+// Аналогично, только с условным оператором ?
+let year = +prompt (`Сколько тебе лет?`, ``);
+
+(year < 5) ? alert (`Привет, малыш!`) :
+(year < 18) ? alert (`Привет!`) :
+(year < 100 ) ? alert (`Здравствуйте`) :
+alert (`Какой странный возраст`);
+
+
+/* Практика № 2 */
+
+let nameJS = prompt (`Какое официальное название JS?`, `ECMAScript`);
+
+if (nameJS === `ECMAScript`) {
+  alert(`Верно`);
+} else {
+  alert (`Не знаете? ECMAScript!`);
+}
+
+/* Практика № 3 */
+
+let value = +prompt (`Введите любое число`, `43`);
+
+if (value > 0) {
+  alert(1);
+} else if (value < 0){
+  alert (-1);
+} else if (value === 0) {
+  alert (0);
+} else {
+  alert (`Надо было вводить цифру!`);
+}
+
+
+/* Практика № 4 */
+
+let result = (value + year < 4) ? `Мало` : `Много`;
