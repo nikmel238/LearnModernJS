12d2b6f0625cd3f54d74f1b46fe01d8797cbcb6b
nikmel238 <man.nikita0@yandex.ru>
2023-01-02 16:03:04 +0300

    add MethodsOfPrimitives

---
+++ LearnModernJS/DataTypes/js/MethodsOfPrimitives.js
@@ -0,0 +1,9 @@
+`use strict`;
+
+const str = `Привет, мир!`;
+const strMeth = str.toUpperCase();
+alert(`Примитив "${str}", используя метод примитива, получили "${strMeth}".`);
+
+const num = 1.12342354;
+const numFix = num.toFixed(2);
+alert(`Примитив "${num}", используя метод примитива, получили "${numFix}".`);
